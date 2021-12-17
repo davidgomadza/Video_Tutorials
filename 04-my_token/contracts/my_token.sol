21e7dfe1e5eb68df09ce81378f04e3043e02b9cc
davidgomadza <49430339+davidgomadza@users.noreply.github.com>
2021-12-17 04:37:22 -0800

    Update my_token.sol

--- 04-my_token/contracts/my_token.sol
+++ 04-my_token/contracts/my_token.sol
@@ -2,8 +2,8 @@
 pragma solidity ^0.8.0;
 
 import "OpenZeppelin/openzeppelin-contracts@4.0.0/contracts/token/ERC20/ERC20.sol";
-
-
+Brownie console -network arbitrum_rinkeby
+>>> TheFellowShip deploy
 contract TheFellowShip is ERC20 {
     constructor(uint256 initialSupply) ERC20("LordOFTheRings", "Tolkien") {
         _mint(msg.sender, initialSupply);
